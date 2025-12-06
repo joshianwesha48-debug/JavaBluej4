f6685e1401166ad2ec49ac6d5663e1c0b283977c
Anwesha joshi <joshianwesha48@gmail.com>
2025-12-06 21:42:35 +0545

    this is the week 5 java

---
+++ week51.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class week51 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class week51
+{
+    public static void main(String[] args){
+        /*for (int i=20; i<=20;i+=2)
+        {
+            System.out.println(i);
+        }*/
+        /*int n=2;
+        while(n<=20){
+            System.out.println(n);
+            n+=2;
+        }*/
+        //do while loop 
+        int n=2;
+        do{
+            System.out.println(n);
+            n+=2;
+        }
+        while(n<=20);
+}
+}
---
+++ week52.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class week52 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class week52
+{
+    public static void main(String[] args){
+        int i = 1;
+        while(i <= 10) {
+            System.out.println(i * i);
+            i++;
+}
+}
+}
